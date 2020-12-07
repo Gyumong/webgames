7538a1020784f0ce5ef67f067a9ab59846a45f71
Gyumong <rlaalsrb3559@naver.com>
2020-12-07 23:28:28 +0900

    틱택토게임

---
+++ 틱택토/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const RefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+module.exports = {
+  name: "word-relay-setting",
+  mode: "development",
+  devtool: "eval",
+  resolve: {
+    extensions: [".js", ".jsx", ".css"],
+  },
+  entry: {
+    app: ["./client"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["@babel/plugin-proposal-class-properties"],
+        },
+      },
+    ],
+  },
+  plugins: [new RefreshWebpackPlugin()],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js",
+  },
+  devServer: {
+    publicPath: "/dist/",
+    hot: true,
+  },
+};
